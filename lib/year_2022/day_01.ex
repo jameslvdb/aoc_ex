cda07355782e0532656d08ad7b57f6a2fcd21140
James Vanderburg <james@vanderburg.org>
2022-12-01 16:02:42 -0600

    Finish Day 1 Part 1

--- lib/year_2022/day_01.ex
+++ lib/year_2022/day_01.ex
@@ -8,8 +8,22 @@ defmodule Year2022.Day01 do
   # Goal: Find the Elf carrying the most Calories.
   # Answer: How many total Calories is that Elf carrying?
 
-  def part_1(inputs) do
+  def part_1(input) do
 	# split by empty line
-    inputs
+
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&split_and_convert/1)
+    |> Enum.map(&reduce_with_sum/1)
+    |> Enum.max()
+  end
+
+  defp split_and_convert(str) do
+    String.split(str, "\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp reduce_with_sum(list) do
+    Enum.reduce(list, 0, fn x, acc -> x + acc end)
   end
 end
