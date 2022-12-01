0ec7b7c8afa0adc10d2ebbaffb69f7a2741a77e9
James Vanderburg <james@vanderburg.org>
2022-12-01 15:36:46 -0600

    Add InputHelper module

---
+++ lib/input_helper.ex
@@ -0,0 +1,22 @@
+defmodule AdventOfCode.InputHelper do
+  @moduledoc """
+  Collects inputs from the correct file.
+  """
+
+  def input_for(year, day) do
+    {:ok, input} =
+    constructed_file_name(year, day)
+    |> File.read()
+
+    String.trim(input)
+  end
+
+  defp constructed_file_name(year, day) do
+    day_val =
+    day
+    |> Integer.to_string()
+    |> String.pad_leading(2, "0")
+
+    "inputs/year_#{year}/day_#{day_val}.txt"
+  end
+end
