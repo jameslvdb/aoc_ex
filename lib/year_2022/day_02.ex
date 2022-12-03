d73826179e8da16d5870b3be482ad0de25f721a9
James Vanderburg <james@vanderburg.org>
2022-12-03 00:25:02 -0600

    Doc comments for day 2

--- lib/year_2022/day_02.ex
+++ lib/year_2022/day_02.ex
@@ -27,7 +27,9 @@ defmodule Year2022.Day02 do
     score_for_result(moves) + score_for_move(moves)
   end
 
-
+  @doc """
+  Get the score the player earns for the result of the match.
+  """
   def score_for_result({move, move}), do: 3
 
   def score_for_result({elf_move, player_move}) do
@@ -39,12 +41,18 @@ defmodule Year2022.Day02 do
     end
   end
 
+  @doc """
+  Get the score the player earns for their move.
+  """
   def score_for_move({_, move}) do
     move_values = %{rock: 1, paper: 2, scissors: 3}
 
     move_values[move]
   end
 
+  @doc """
+  Map characters to both players' corresponding moves.
+  """
   def get_moves([elf_move, player_move]) do
     elf_moves = %{"A" => :rock, "B" => :paper, "C" => :scissors}
     player_moves = %{"X" => :rock, "Y" => :paper, "Z" => :scissors}
@@ -52,17 +60,26 @@ defmodule Year2022.Day02 do
     {elf_moves[elf_move], player_moves[player_move]}
   end
 
+  @doc """
+  Convert A, B, or C into the elf's move.
+  """
   def get_elf_move([elf_move, intended_result_key]) do
     elf_moves = %{"A" => :rock, "B" => :paper, "C" => :scissors}
 
     {elf_moves[elf_move], intended_result_key}
   end
 
+  @doc """
+  Convert X, Y, or Z into the intended result of the round.
+  """
   def get_intended_result({elf_move, intended_result_key}) do
     results = %{"X" => :loss, "Y" => :draw, "Z" => :win}
     {elf_move, results[intended_result_key]}
   end
 
+  @doc """
+  Gets the correct move against your opponent based on the intended result.
+  """
   def get_correct_move({elf_move, intended_result}) do
     case {elf_move, intended_result} do
       {_, :win} -> find_winning_move(elf_move)
@@ -71,12 +88,21 @@ defmodule Year2022.Day02 do
     end
   end
 
+  @doc """
+  Find a move that results in a win against your opponent.
+  """
   def find_winning_move(:rock), do: {:rock, :paper}
   def find_winning_move(:paper), do: {:paper, :scissors}
   def find_winning_move(:scissors), do: {:scissors, :rock}
 
+  @doc """
+  Find a move that results in a draw with your opponent.
+  """
   def find_drawing_move(move), do: {move, move}
 
+  @doc """
+  Find a move that results in a loss against your opponent.
+  """
   def find_losing_move(:rock), do: {:rock, :scissors}
   def find_losing_move(:paper), do: {:paper, :rock}
   def find_losing_move(:scissors), do: {:scissors, :paper}
