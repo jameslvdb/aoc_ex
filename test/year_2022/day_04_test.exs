566a4b05feabe0370cf511c39d212165e7279564
James Vanderburg <james@vanderburg.org>
2022-12-05 00:59:26 -0600

    mix format

--- test/year_2022/day_04_test.exs
+++ test/year_2022/day_04_test.exs
@@ -29,21 +29,21 @@ defmodule Year2022.Day04Test do
   end
 
   test "create_ranges" do
-	assert Day04.create_ranges("4-6") == Range.new(4, 6)
+    assert Day04.create_ranges("4-6") == Range.new(4, 6)
   end
 
   test "check ranges" do
-	assert Day04.check_ranges([1..20, 4..8]) == true
-	assert Day04.check_ranges([6..20, 1..30]) == true
-	assert Day04.check_ranges([4..6, 6..6]) == true
+    assert Day04.check_ranges([1..20, 4..8]) == true
+    assert Day04.check_ranges([6..20, 1..30]) == true
+    assert Day04.check_ranges([4..6, 6..6]) == true
 
-	assert Day04.check_ranges([4..6, 6..8]) == false
-	assert Day04.check_ranges([4..6, 5..8]) == false
-	assert Day04.check_ranges([4..6, 10..15]) == false
-	assert Day04.check_ranges([5..8, 1..3]) == false
+    assert Day04.check_ranges([4..6, 6..8]) == false
+    assert Day04.check_ranges([4..6, 5..8]) == false
+    assert Day04.check_ranges([4..6, 10..15]) == false
+    assert Day04.check_ranges([5..8, 1..3]) == false
   end
 
   test "chack range overlap" do
-	assert Day04.check_range_overlap([1..10, 5..15]) == true
+    assert Day04.check_range_overlap([1..10, 5..15]) == true
   end
 end
