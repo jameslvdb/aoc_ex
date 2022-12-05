3c20631af93cc10978c1fd8a481eb629b0ae44d9
James Vanderburg <james@vanderburg.org>
2022-12-05 17:59:15 -0600

    Day 5 Part 2

--- lib/year_2022/day_05.ex
+++ lib/year_2022/day_05.ex
@@ -19,6 +19,26 @@ defmodule Year2022.Day05 do
     |> Enum.join()
   end
 
+  def full_part_2() do
+    File.read!("inputs/year_2022/day_05.txt")
+    |> split_lines()
+    |> part_2()
+  end
+
+  def part_2(lines) do
+    # create data structure to represent state of crates
+    stacks =
+      take_crate_lines(lines)
+      |> build_stacks()
+
+    take_move_lines(lines)
+    |> Enum.reject(fn x -> x == "" || x == nil end)
+    |> Enum.map(&build_move/1)
+    |> Enum.reduce(stacks, fn x, acc -> perform_cratemover_9001_move(x, acc) end)
+    |> Enum.map(&hd/1)
+    |> Enum.join()
+  end
+
   @doc """
   Builds the initial representation of the stacks of crates.
   """
@@ -37,6 +57,14 @@ defmodule Year2022.Day05 do
     reversed_crates = Enum.reverse(crates_to_move)
 
     List.replace_at(stacks, to, reversed_crates ++ Enum.at(stacks, to))
+    |> List.replace_at(from, remaining_crates)
+  end
+
+  def perform_cratemover_9001_move(move, stacks) do
+    [count, from, to] = Map.values(move)
+    {crates_to_move, remaining_crates} = Enum.split(Enum.at(stacks, from), count)
+
+    List.replace_at(stacks, to, crates_to_move ++ Enum.at(stacks, to))
     |> List.replace_at(from, remaining_crates)
   end
 
