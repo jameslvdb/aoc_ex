009e004a61183189416f7ffee60d6ed40574d604
James Vanderburg <james@vanderburg.org>
2022-12-05 23:57:00 -0600

    Day 6 Part 2

--- lib/year_2022/day_06.ex
+++ lib/year_2022/day_06.ex
@@ -1,15 +1,25 @@
 defmodule Year2022.Day06 do
   def part_1(str) do
+    find_packet(str, 4)
+  end
+
+  def part_2(str) do
+    find_packet(str, 14)
+  end
+
+  def find_packet(str, num_uniq_chars) do
     chars = String.graphemes(str)
     range = 0..(length(chars) - 1)
 
     Enum.reduce_while(range, 0, fn i, _acc ->
-      if next_four_elements(chars, i) == 4, do: {:halt, i}, else: {:cont, 0}
-    end) + 4
+      if next_elements(chars, i, num_uniq_chars) == num_uniq_chars,
+        do: {:halt, i},
+        else: {:cont, 0}
+    end) + num_uniq_chars
   end
 
-  def next_four_elements(chars, index) do
-    Enum.slice(chars, index, 4)
+  def next_elements(chars, index, count) do
+    Enum.slice(chars, index, count)
     |> Enum.uniq()
     |> Enum.count()
   end
--- test/year_2022/day_06_test.exs
+++ test/year_2022/day_06_test.exs
@@ -21,6 +21,22 @@ defmodule Year2022.Day06Test do
   end
 
   test "finds the start of message marker" do
+    str_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    str_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    str_3 = "nppdvjthqldpwncqszvftbrmjlhg"
+    str_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    str_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
+    assert Day06.part_2(str_1) == 19
+    assert Day06.part_2(str_2) == 23
+    assert Day06.part_2(str_3) == 23
+    assert Day06.part_2(str_4) == 29
+    assert Day06.part_2(str_5) == 26
+  end
+
+  test "full part 2" do
+    File.read!("inputs/year_2022/day_06.txt")
+    |> Day06.part_2()
+    |> dbg()
   end
 end
