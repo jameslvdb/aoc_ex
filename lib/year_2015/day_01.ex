743afd4888e6a86432404f817b8268fef038d36d
James Vanderburg <james@vanderburg.org>
2022-12-06 13:43:18 -0600

    2015 Day 1 Part 2

--- lib/year_2015/day_01.ex
+++ lib/year_2015/day_01.ex
@@ -4,18 +4,29 @@ defmodule Year2015.Day01 do
     |> Enum.reduce(0, fn x, acc -> acc + paren_value(x) end)
   end
 
+  def part_2(input) do
+    String.graphemes(input)
+    |> Enum.reduce_while(%{floor: 0, index: 0}, fn paren, state ->
+      if state.floor + paren_value(paren) == -1,
+        do: {:halt, state.index + 1},
+        else: {:cont, %{floor: state.floor + paren_value(paren), index: state.index + 1}}
+    end)
+  end
+
   def paren_value(char) do
     case char do
-      "(" ->
-        1
-
-      ")" ->
-        -1
+      "(" -> 1
+      ")" -> -1
     end
   end
 
   def full_part_1() do
     InputHelper.input_for(2015, 1)
     |> part_1()
+  end
+
+  def full_part_2() do
+    InputHelper.input_for(2015, 1)
+    |> part_2()
   end
 end
--- test/year_2015/day_01_test.exs
+++ test/year_2015/day_01_test.exs
@@ -12,4 +12,9 @@ defmodule Year2015.Day01Test do
     assert Day01.part_1("))(") == -1
     assert Day01.part_1(")))") == -3
   end
+
+  test "part 2" do
+    assert Day01.part_2(")") == 1
+    assert Day01.part_2("()())") == 5
+  end
 end
