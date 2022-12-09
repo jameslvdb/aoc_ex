c14af5ad2d842efdbacbeab0c9efda297f3ed841
James Vanderburg <james@vanderburg.org>
2022-12-09 17:11:01 -0600

    Day 8 Part 2

--- lib/year_2022/day_08.ex
+++ lib/year_2022/day_08.ex
@@ -1,6 +1,6 @@
 defmodule Year2022.Day08 do
   def full_part_1() do
-	InputHelper.input_for(2022, 8)
+    InputHelper.input_for(2022, 8)
     |> String.split("\n")
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(fn line -> Enum.map(line, &String.to_integer/1) end)
@@ -10,18 +10,21 @@ defmodule Year2022.Day08 do
   def part_1(tree_matrix) do
     transposed_trees = transpose(tree_matrix)
 
-    trees_visible_horizontally = visible_trees_in_matrix(tree_matrix)
-    |> MapSet.new()
-    trees_visible_vertically = visible_trees_in_matrix(transposed_trees)
-    |> Enum.map(fn {x, y} -> {y, x} end)
-    |> MapSet.new()
+    trees_visible_horizontally =
+      visible_trees_in_matrix(tree_matrix)
+      |> MapSet.new()
+
+    trees_visible_vertically =
+      visible_trees_in_matrix(transposed_trees)
+      |> Enum.map(fn {x, y} -> {y, x} end)
+      |> MapSet.new()
 
     MapSet.union(trees_visible_horizontally, trees_visible_vertically)
     |> Enum.count()
   end
 
   def visible_trees_in_matrix(matrix) do
-	Enum.with_index(matrix)
+    Enum.with_index(matrix)
     |> Enum.map(fn {line, outer_index} -> visible_trees_in_line(line, outer_index) end)
     |> List.flatten()
   end
@@ -35,11 +38,6 @@ defmodule Year2022.Day08 do
       end)
 
     Enum.sort(visible)
-  end
-
-  defp transpose(matrix) do
-    List.zip(matrix)
-    |> Enum.map(&Tuple.to_list/1)
   end
 
   def visible?(_line, 0), do: true
@@ -56,6 +54,64 @@ defmodule Year2022.Day08 do
 
   defp visible_right?(right, tree) do
     Enum.all?(right, fn x -> x < tree end)
+  end
+
+  def full_part_2() do
+    InputHelper.input_for(2022, 8)
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(fn line -> Enum.map(line, &String.to_integer/1) end)
+    |> part_2()
+  end
+
+  def part_2(matrix) do
+    scenic_scores_for_matrix(matrix)
+    |> List.flatten()
+    |> Enum.max()
+  end
+
+  def scenic_scores_for_matrix(matrix) do
+    transposed_trees = transpose(matrix)
+
+    scores =
+      Enum.map(matrix, &scenic_scores_for_line/1)
+      |> dbg()
+
+    transposed_scores =
+      Enum.map(transposed_trees, &scenic_scores_for_line/1)
+      |> dbg()
+
+    Enum.map(0..(length(scores) - 1), fn i ->
+      Enum.zip(Enum.at(transpose(scores), i), Enum.at(transposed_scores, i))
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&List.flatten/1)
+      |> Enum.map(fn list -> Enum.reduce(list, fn x, acc -> x * acc end) end)
+    end)
+    |> dbg()
+  end
+
+  def scenic_scores_for_line(line) do
+    Enum.map(0..(length(line) - 1), fn tree ->
+      scenic_score(line, tree)
+    end)
+  end
+
+  defp transpose(matrix) do
+    List.zip(matrix)
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def scenic_score(line, pos) do
+    {left, tree, right} = split_around(line, pos)
+
+    [ scenic_score_side(Enum.reverse(left), tree), scenic_score_side(right, tree) ]
+  end
+
+  defp scenic_score_side([], _), do: 0
+
+  defp scenic_score_side(trees, tree) do
+    index = Enum.find_index(trees, fn x -> x >= tree end)
+    if is_nil(index), do: length(trees), else: index + 1
   end
 
   defp split_around(line, position) do
--- test/year_2022/day_08_test.exs
+++ test/year_2022/day_08_test.exs
@@ -33,18 +33,33 @@ defmodule Year2022.Day08Test do
     assert Day08.part_1(test_input) == 21
   end
 
-  test "transposing" do
-    Day08.visible_trees_in_matrix(transposed_input)
-    |> Enum.map(fn {x, y} -> {y, x} end)
-    |> dbg()
-  end
-
   test "single line" do
     assert Day08.visible_trees_in_line([3, 0, 3, 7, 3], 0) == [{0, 0}, {0, 3}, {0, 4}]
     assert Day08.visible_trees_in_line([2, 5, 5, 1, 2], 0) == [{0, 0}, {0, 1}, {0, 2}, {0, 4}]
     assert Day08.visible_trees_in_line([6, 5, 4, 4, 2], 0) == [{0, 0}, {0, 1}, {0, 3}, {0, 4}]
     assert Day08.visible_trees_in_line([3, 3, 5, 4, 9], 0) == [{0, 0}, {0, 2}, {0, 4}]
     assert Day08.visible_trees_in_line([3, 5, 3, 9, 0], 0) == [{0, 0}, {0, 1}, {0, 3}, {0, 4}]
+  end
+
+  test "part 2" do
+    assert Day08.part_2(test_input) == 8
+  end
+
+  test "scenic score - single tree" do
+    assert Day08.scenic_score([1, 3, 2, 3, 0], 3) == [2, 1]
+    assert Day08.scenic_score([1, 3, 2, 3, 0], 0) == [0, 1]
+    assert Day08.scenic_score([1, 6, 2, 3, 4], 4) == [3, 0]
+    assert Day08.scenic_score([1, 3, 2, 3, 0], 4) == [1, 0]
+  end
+
+  test "scenic score - single line" do
+    assert Day08.scenic_scores_for_line([1, 3, 2, 3, 0]) == [
+             [0, 1],
+             [1, 2],
+             [1, 1],
+             [2, 1],
+             [1, 0]
+           ]
   end
 
   def test_input() do
