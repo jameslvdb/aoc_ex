e0fa8e797583b790475883198b39dbcf8067f8f9
James Vanderburg <james@vanderburg.org>
2022-12-10 22:56:32 -0600

    Refactor part 1

--- lib/year_2022/day_09.ex
+++ lib/year_2022/day_09.ex
@@ -11,45 +11,83 @@ defmodule Year2022.Day09 do
         head: %{x: 0, y: 0},
         tail: %{x: 0, y: 0}
       },
-      visited: MapSet.new([%{x: 0, y: 0}])
+      head_list: [],
+      tail_moves: [],
+      tail_list: [%{x: 0, y: 0}]
     }
 
-    result =
-      Enum.map(lines, fn line ->
-        [direction, steps] = String.split(line)
-        [direction, String.to_integer(steps)]
-      end)
-      |> Enum.reduce(data, fn line, acc -> process_move(acc, line) end)
+    head_move_list =
+      Enum.map(lines, &line_to_move_list/1)
+      |> List.flatten()
 
-    Enum.count(result.visited)
+    result = Enum.reduce(head_move_list, data, fn line, acc -> process_move(acc, line) end)
+
+    Enum.uniq(result.tail_list)
+    |> Enum.count()
   end
 
-  def process_move(data, ["R", steps]) do
-    Enum.reduce(1..steps, data, fn _step, acc ->
-      update_in(acc.coords.head.x, &(&1 + 1))
-      |> check_tail("R")
-    end)
+  def part_2(lines) do
+    data = %{
+      coords: %{
+        head: %{x: 0, y: 0},
+        tail: %{x: 0, y: 0}
+      },
+      head_list: [],
+      tail_moves: [],
+      tail_list: [%{x: 0, y: 0}]
+    }
+
+    head_move_list =
+      Enum.map(lines, &line_to_move_list/1)
+      |> Enum.flatten()
   end
 
-  def process_move(data, ["L", steps]) do
-    Enum.reduce(1..steps, data, fn _step, acc ->
-      update_in(acc.coords.head.x, &(&1 - 1))
-      |> check_tail("L")
-    end)
+  def line_to_move_list(line) do
+    [direction, steps] = String.split(line)
+
+    steps = String.to_integer(steps)
+
+    case direction do
+      "R" ->
+        Enum.map(1..steps, fn _x -> %{x: 1, y: 0} end)
+
+      "L" ->
+        Enum.map(1..steps, fn _x -> %{x: -1, y: 0} end)
+
+      "U" ->
+        Enum.map(1..steps, fn _x -> %{x: 0, y: 1} end)
+
+      "D" ->
+        Enum.map(1..steps, fn _x -> %{x: 0, y: -1} end)
+    end
   end
 
-  def process_move(data, ["D", steps]) do
-    Enum.reduce(1..steps, data, fn _step, acc ->
-      update_in(acc.coords.head.y, &(&1 - 1))
-      |> check_tail("D")
+  def build_head_move_list(lines, data) do
+    Enum.map(lines, fn line ->
+      [direction, steps] = String.split(line)
+      [direction, String.to_integer(steps)]
     end)
+    |> Enum.reduce(data, fn line, acc -> process_move(acc, line) end)
   end
 
-  def process_move(data, ["U", steps]) do
-    Enum.reduce(1..steps, data, fn _step, acc ->
-      update_in(acc.coords.head.y, &(&1 + 1))
-      |> check_tail("U")
-    end)
+  def process_move(data, %{x: 1, y: 0}) do
+    update_in(data.coords.head.x, &(&1 + 1))
+    |> check_tail("R")
+  end
+
+  def process_move(data, %{x: -1, y: 0}) do
+    update_in(data.coords.head.x, &(&1 - 1))
+    |> check_tail("L")
+  end
+
+  def process_move(data, %{x: 0, y: -1}) do
+    update_in(data.coords.head.y, &(&1 - 1))
+    |> check_tail("D")
+  end
+
+  def process_move(data, %{x: 0, y: 1}) do
+    update_in(data.coords.head.y, &(&1 + 1))
+    |> check_tail("U")
   end
 
   def check_tail(data, move_direction) do
@@ -65,7 +103,8 @@ defmodule Year2022.Day09 do
     new_tail = %{x: head_x + 1, y: head_y}
 
     data = put_in(data.coords.tail, new_tail)
-    put_in(data.visited, MapSet.put(data.visited, new_tail))
+    data = put_in(data.tail_list, [new_tail | data.tail_list])
+    put_in(data.tail_moves, [%{x: -1, y: 0} | data.tail_moves])
   end
 
   def update_tail(data, "R") do
@@ -73,7 +112,8 @@ defmodule Year2022.Day09 do
     new_tail = %{x: head_x - 1, y: head_y}
 
     data = put_in(data.coords.tail, new_tail)
-    put_in(data.visited, MapSet.put(data.visited, new_tail))
+    data = put_in(data.tail_list, [new_tail | data.tail_list])
+    put_in(data.tail_moves, [%{x: 1, y: 0} | data.tail_moves])
   end
 
   def update_tail(data, "D") do
@@ -81,7 +121,8 @@ defmodule Year2022.Day09 do
     new_tail = %{x: head_x, y: head_y + 1}
 
     data = put_in(data.coords.tail, new_tail)
-    put_in(data.visited, MapSet.put(data.visited, new_tail))
+    data = put_in(data.tail_list, [new_tail | data.tail_list])
+    put_in(data.tail_moves, [%{x: 0, y: -1} | data.tail_moves])
   end
 
   def update_tail(data, "U") do
@@ -89,7 +130,8 @@ defmodule Year2022.Day09 do
     new_tail = %{x: head_x, y: head_y - 1}
 
     data = put_in(data.coords.tail, new_tail)
-    put_in(data.visited, MapSet.put(data.visited, new_tail))
+    data = put_in(data.tail_list, [new_tail | data.tail_list])
+    put_in(data.tail_moves, [%{x: 0, y: 1} | data.tail_moves])
   end
 
   def move_tail?(head_coords, tail_coords) do
--- test/year_2022/day_09_test.exs
+++ test/year_2022/day_09_test.exs
@@ -17,4 +17,60 @@ defmodule Year2022.Day09Test do
     Day09.part_1(input)
     |> dbg()
   end
+
+  test "part_2" do
+    input = [
+      "R 5",
+      "U 8",
+      "L 8",
+      "D 3",
+      "R 17",
+      "D 10",
+      "L 25",
+      "U 20"
+    ]
+
+    Day09.part_2(input)
+    |> dbg()
+  end
+
+  describe "part 2" do
+    setup do
+      {:ok,
+       data: %{
+         coords: %{
+           head: %{x: 0, y: 0},
+           tail: %{x: 0, y: 0}
+         },
+         head_list: [],
+         visited: MapSet.new([%{x: 0, y: 0}])
+       }}
+    end
+
+    test "it processes a right move", context do
+      assert Day09.process_move(context[:data], %{x: 1, y: 0}).head_list == [%{x: 1, y: 0}]
+    end
+
+    test "it processes a left move", context do
+      assert Day09.process_move(context[:data], %{x: -1, y: 0}).head_list == [%{x: -1, y: 0}]
+    end
+
+    test "it processes an up move", context do
+      assert Day09.process_move(context[:data], %{x: 0, y: 1}).head_list == [%{x: 0, y: 1}]
+    end
+
+    test "it processes a down move", context do
+      assert Day09.process_move(context[:data], %{x: 0, y: -1}).head_list == [%{x: 0, y: -1}]
+    end
+  end
+
+  describe "line to move list" do
+    test "move right" do
+      assert Day09.line_to_move_list("R 3") == [
+               %{x: 1, y: 0},
+               %{x: 1, y: 0},
+               %{x: 1, y: 0}
+             ]
+    end
+  end
 end
