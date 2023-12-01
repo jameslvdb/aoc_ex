e15bf656f353827556ad735c6813ccba9426c4b1
James Vanderburg <james@vanderburg.org>
2023-11-30 23:03:30 -0600

    Add clojure project, download 2023/01

--- clojure/advent.clj
+++ clojure/advent.clj
@@ -1,15 +1,1 @@
-(defn hello-world []
-  (println "Hello, World!"))
 
-(defn advent-of-code []
-  (println "Advent of Code!")
-  (println "Which year?")
-  (let [year (read-line)]
-    (println "Which day?")
-    (let [day (read-line)]
-      (println "Which part?")
-      (let [part (read-line)]
-        (println (format "Year: %s, Day: %s, Part: %s" year day part))))))
-
-(hello-world)
-(advent-of-code)
---
+++ clojure/aoc/src/aoc/year_2022/day_01.clj
@@ -0,0 +1,3 @@
+(ns aoc.year-2022.day-01)
+
+(defn part-1 [lines])
