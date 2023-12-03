5b80966a30a799f3bb904815366867bfc2924d43
James Vanderburg <james@vanderburg.org>
2023-12-03 15:59:36 -0600

    Complete 2023 day 3 part 1 in elixir

--- elixir/lib/year_2023/day_03.ex
+++ elixir/lib/year_2023/day_03.ex
@@ -1,4 +1,8 @@
 defmodule Year2023.Day03 do
+  @symbol_regex ~r/[^\d\.]/
+  @number_regex ~r/\d+/
+  @gear_regex ~r/\*/
+
   def full_part_1() do
     InputHelper.input_for(2023, 3)
     |> String.split("\n")
@@ -6,7 +10,26 @@ defmodule Year2023.Day03 do
   end
 
   def part_1(lines) do
-    nil
+    sum_of_numbers = 0
+    number_indices = Enum.map(lines, &find_numbers/1)
+
+    # Format is:
+    # {[[{0, 3}], [{5, 3}]], 0}
+    # {[], 1}
+    # {[[{2, 2}], [{6, 3}]], 2}
+    # {[], 3}
+    # {[[{0, 3}]], 4}
+    # {[[{7, 2}]], 5}
+    # {[[{2, 3}]], 6}
+    # {[[{6, 3}]], 7}
+    # {[], 8}
+    # {[[{1, 3}], [{5, 3}]], 9}
+    # {indices, matrix_index}
+
+    Enum.with_index(number_indices)
+    |> Enum.map(fn x -> check_line(x, lines) end)
+    |> List.flatten()
+    |> Enum.sum()
   end
 
   def full_part_2() do
@@ -17,5 +40,76 @@ defmodule Year2023.Day03 do
 
   def part_2(lines) do
     nil
+  end
+
+  def find_numbers(line) do
+    Regex.scan(@number_regex, line, return: :index)
+  end
+
+  def check_line({indices, line_index}, matrix) do
+    Enum.map(indices, fn pair -> check_surrounding(pair, line_index, matrix) end)
+  end
+
+  def check_surrounding([{index, length}], line_index, matrix) do
+    line = Enum.at(matrix, line_index)
+    number = String.slice(line, index, length)
+
+    chars =
+      surrounding_chars({line_index, {index, length}}, matrix)
+      |> Enum.join()
+
+    if String.match?(chars, @symbol_regex), do: String.to_integer(number), else: 0
+  end
+
+  def surrounding_chars({row_index, {index, length}}, matrix) do
+    cond do
+      row_index == 0 ->
+        Enum.concat([
+          current_row_chars({row_index, index, length}, matrix),
+          below_row_chars({row_index, index, length}, matrix)
+        ])
+
+      row_index == Enum.count(matrix) - 1 ->
+        Enum.concat([
+          above_row_chars({row_index, index, length}, matrix),
+          current_row_chars({row_index, index, length}, matrix)
+        ])
+
+      true ->
+        Enum.concat([
+          above_row_chars({row_index, index, length}, matrix),
+          current_row_chars({row_index, index, length}, matrix),
+          below_row_chars({row_index, index, length}, matrix)
+        ])
+    end
+  end
+
+  def above_row_chars({row_index, column_index, length}, matrix) do
+    row = Enum.at(matrix, row_index - 1)
+
+    substring = String.slice(row, column_index - 1, length + 2)
+    String.graphemes(substring)
+  end
+
+  def below_row_chars({row_index, column_index, length}, matrix) do
+    row = Enum.at(matrix, row_index + 1)
+    # substring = String.slice(row, column_index - 1, length + 2)
+    substring = find_chars(row, column_index - 1, length)
+    String.graphemes(substring)
+  end
+
+  def current_row_chars({row_index, column_index, length}, matrix) do
+    row = Enum.at(matrix, row_index)
+    char_before = String.at(row, column_index - 1)
+    char_after = String.at(row, column_index + length)
+    [char_before, char_after]
+  end
+
+  defp find_chars(string, -1, length) do
+    String.slice(string, 0, length + 1)
+  end
+
+  defp find_chars(string, index, length) do
+    String.slice(string, index, length + 2)
   end
 end
--- elixir/test/year_2023/day_03_test.exs
+++ elixir/test/year_2023/day_03_test.exs
@@ -4,9 +4,89 @@ defmodule Year2023.Day03Test do
 
   describe "example cases" do
     test "it gets the correct result for part 1" do
+      lines =
+        InputHelper.example_input_for(2023, 3, 1)
+        |> String.split("\n")
+
+      assert Day03.part_1(lines) == 4361
     end
 
     test "it gets the correct result for part 2" do
     end
+  end
+
+  test "it gets index and length of every number" do
+    str = ".34..*.4"
+    assert Day03.find_numbers(str) == [[{1, 2}], [{7, 1}]]
+  end
+
+  test "check_surrounding" do
+    matrix_with_symbol = [
+      "..*",
+      ".9.",
+      "..."
+    ]
+
+    assert Day03.check_surrounding([{1, 1}], 1, matrix_with_symbol) == 9
+  end
+
+  @tag :skip
+  test "check_line" do
+    matrix_with_symbol = [
+      "..*",
+      ".9.",
+      "..."
+    ]
+
+    indices_with_index =
+      Enum.map(matrix_with_symbol, &Day03.find_numbers/1)
+      |> Enum.with_index()
+
+    IO.inspect(Enum.map(indices_with_index, &Day03.check_line(&1, matrix_with_symbol)))
+  end
+
+  test "surrounding_chars" do
+    matrix = [
+      "..$....*....%.",
+      "...56.........",
+      "....#........."
+    ]
+
+    assert Day03.surrounding_chars({1, {3, 2}}, matrix) == ~w($ . . . . . . . # .)
+  end
+
+  test "above_row_chars" do
+    matrix = [
+      "..$....*....%.",
+      "...56........."
+    ]
+
+    coords = {1, 3, 2}
+
+    assert Day03.above_row_chars(coords, matrix) == ~w($ . . .)
+  end
+
+  test "below_row_chars" do
+    matrix = [
+      "..$....*....%.",
+      "...56.........",
+      "....#........."
+    ]
+
+    coords = {1, 3, 2}
+
+    assert Day03.below_row_chars(coords, matrix) == ~w(. . # .)
+  end
+
+  test "current_row_chars" do
+    matrix = [
+      "..$....*....%.",
+      "...56.........",
+      "....#........."
+    ]
+
+    coords = {1, 3, 2}
+
+    assert Day03.current_row_chars(coords, matrix) == ~w(. .)
   end
 end
