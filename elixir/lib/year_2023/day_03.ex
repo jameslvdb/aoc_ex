a737ecdec096a39d8269ff8f776a1e115ced040c
James Vanderburg <james@vanderburg.org>
2023-12-02 21:48:58 -0600

    Set up day 3

---
+++ elixir/lib/year_2023/day_03.ex
@@ -0,0 +1,21 @@
+defmodule Year2023.Day03 do
+  def full_part_1() do
+    InputHelper.input_for(2023, 3)
+    |> String.split("\n")
+    |> part_1()
+  end
+
+  def part_1(lines) do
+    nil
+  end
+
+  def full_part_2() do
+    InputHelper.input_for(2023, 3)
+    |> String.split("\n")
+    |> part_2()
+  end
+
+  def part_2(lines) do
+    nil
+  end
+end
---
+++ elixir/test/year_2023/day_03_test.exs
@@ -0,0 +1,7 @@
+defmodule Year2023.Day03Test do
+  alias Year2023.Day03
+  use ExUnit.Case
+
+  describe "example cases" do
+  end
+end
