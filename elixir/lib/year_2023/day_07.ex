859db8484cea53a0e967a4a21797bbb669e43169
James Vanderburg <james@vanderburg.org>
2023-12-07 22:25:52 -0600

    [ex] Finish 2023 Day 7 Part 2

--- elixir/lib/year_2023/day_07.ex
+++ elixir/lib/year_2023/day_07.ex
@@ -1,6 +1,8 @@
 defmodule Year2023.Day07 do
   @card_order ~w(2 3 4 5 6 7 8 9 T J Q K A)
 
+  @joker_card_order ~w(J 2 3 4 5 6 7 8 9 T Q K A)
+
   @hand_values %{
     five: 6,
     four: 5,
@@ -19,7 +21,20 @@ defmodule Year2023.Day07 do
   def part_1(input) do
     input
     |> parse_input()
-    |> Enum.map(&enrich_with_sort_structure/1)
+    |> Enum.map(&enrich_with_sort_structure(&1, :p1))
+    |> Enum.sort(&(&1.sort_structure < &2.sort_structure))
+    |> total_winnings()
+  end
+
+  def full_part_2() do
+    InputHelper.input_for(2023, 7)
+    |> part_2()
+  end
+
+  def part_2(input) do
+    input
+    |> parse_input()
+    |> Enum.map(&enrich_with_sort_structure(&1, :p2))
     |> Enum.sort(&(&1.sort_structure < &2.sort_structure))
     |> total_winnings()
   end
@@ -39,20 +54,30 @@ defmodule Year2023.Day07 do
   end
 
   defp hand_winnings({hand, index}) do
-    dbg({hand.hand, hand.bid, index})
     hand.bid * (index + 1)
   end
 
-  def enrich_with_sort_structure(hand_map) do
+  def enrich_with_sort_structure(hand_map, :p1) do
     structure = {
-      @hand_values[hand_type(hand_map.hand)],
-      card_values(hand_map.hand)
+      @hand_values[hand_type(hand_map.hand, :p1)],
+      card_values(hand_map.hand, @card_order)
     }
 
     Map.put(hand_map, :sort_structure, structure)
   end
 
-  def hand_type(hand) do
+  def enrich_with_sort_structure(hand_map, :p2) do
+    hand_type = hand_type(hand_map.hand, :p2)
+
+    structure = {
+      @hand_values[hand_type],
+      card_values(hand_map.hand, @joker_card_order)
+    }
+
+    Map.put(hand_map, :sort_structure, structure)
+  end
+
+  def hand_type(hand, :p1) do
     card_counts =
       String.graphemes(hand)
       |> Enum.frequencies()
@@ -83,8 +108,28 @@ defmodule Year2023.Day07 do
     end
   end
 
-  def card_values(str) do
+  def hand_type("JJJJJ", _), do: :five
+
+  def hand_type(hand, :p2) do
+    has_joker? = String.contains?(hand, "J")
+
+    if has_joker? do
+      # turn joker(s) into card with the most occurrences
+      {elem, _} =
+        String.graphemes(hand)
+        |> Enum.frequencies()
+        |> Map.delete("J")
+        |> Enum.max_by(fn {_k, v} -> v end)
+
+      String.replace(hand, "J", elem)
+      |> hand_type(:p1)
+    else
+      hand_type(hand, :p1)
+    end
+  end
+
+  def card_values(str, card_order) do
     String.graphemes(str)
-    |> Enum.map(fn my_card -> Enum.find_index(@card_order, fn x -> x == my_card end) end)
+    |> Enum.map(fn my_card -> Enum.find_index(card_order, fn x -> x == my_card end) end)
   end
 end
--- elixir/test/year_2023/day_07_test.exs
+++ elixir/test/year_2023/day_07_test.exs
@@ -2,7 +2,12 @@ defmodule Year2023.Day07Test do
   alias Year2023.Day07
   use ExUnit.Case
 
-  test "part 1 example " do
+  @card_order ~w(2 3 4 5 6 7 8 9 T J Q K A)
+
+  @joker_card_order ~w(J 2 3 4 5 6 7 8 9 T Q K A)
+
+  @tag :skip
+  test "part 1 example" do
     input = """
     32T3K 765
     T55J5 684
@@ -14,8 +19,20 @@ defmodule Year2023.Day07Test do
     assert Day07.part_1(input) == 6440
   end
 
+  test "part 2 example" do
+    input = """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+
+    assert Day07.part_2(input) == 5905
+  end
+
   test "card_values" do
-    assert Day07.card_values("2359Q") == [0, 1, 3, 7, 10]
+    assert Day07.card_values("2359Q", @card_order) == [0, 1, 3, 7, 10]
   end
 
   test "card values sorting" do
@@ -24,16 +41,23 @@ defmodule Year2023.Day07Test do
     rank_4 = "T55J5"
     rank_5 = "QQQJA"
 
-    assert Day07.card_values(rank_2) < Day07.card_values(rank_3)
-    assert Day07.card_values(rank_4) < Day07.card_values(rank_5)
+    assert Day07.card_values(rank_2, @card_order) < Day07.card_values(rank_3, @card_order)
+    assert Day07.card_values(rank_4, @card_order) < Day07.card_values(rank_5, @card_order)
   end
 
   test "hand_type" do
-    assert Day07.hand_type("AAAAA") == :five
-    assert Day07.hand_type("44444") == :five
-    assert Day07.hand_type("AAAA8") == :four
-    assert Day07.hand_type("44455") == :full_house
-    assert Day07.hand_type("56595") == :three
-    assert Day07.hand_type("5588A") == :two_pair
+    assert Day07.hand_type("AAAAA", :p1) == :five
+    assert Day07.hand_type("44444", :p1) == :five
+    assert Day07.hand_type("AAAA8", :p1) == :four
+    assert Day07.hand_type("44455", :p1) == :full_house
+    assert Day07.hand_type("56595", :p1) == :three
+    assert Day07.hand_type("5588A", :p1) == :two_pair
+  end
+
+  test "part 2 hand type" do
+    assert Day07.hand_type("AAAAA", :p2) == :five
+
+    assert Day07.hand_type("AAAAJ", :p2) == :five
+    assert Day07.hand_type("AAJKK", :p2) == :full_house
   end
 end
