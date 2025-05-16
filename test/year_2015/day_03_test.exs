3347d779d8c6cfb1b6509fe71be54e045f836595
James Vanderburg <james@vanderburg.org>
2025-05-16 15:51:09 -0500

    Fix 2015 day 3 tests

--- test/year_2015/day_03_test.exs
+++ test/year_2015/day_03_test.exs
@@ -5,55 +5,52 @@ defmodule Year2015.Day03Test do
 
   test "update_state" do
     state = %{
-      houses: %{
-        {0, 0} => 1
-      },
+      houses: MapSet.new([{0, 0}]),
       position: {0, 0}
     }
 
-    assert state == %{
-             houses: %{{0, 0} => 1},
-             position: {0, 0}
-           }
-
     assert Day03.update_state({0, 0}, state).position == {0, 0}
-    assert Day03.update_state({0, 0}, state).houses == %{{0, 0} => 2}
+    assert Day03.update_state({0, 0}, state).houses == mapset([{0, 0}])
     assert Day03.update_state({1, 0}, state).position == {1, 0}
-    assert Day03.update_state({1, 0}, state).houses == %{{0, 0} => 1, {1, 0} => 1}
+    assert Day03.update_state({1, 0}, state).houses == mapset([{0, 0}, {1, 0}])
   end
 
   describe "process move" do
     setup [:setup_state]
 
     test "an up move", context do
-      assert Day03.process_move("^", context.state).position == {0, 1}
-      assert Day03.process_move("^", context.state).houses == %{{0, 0} => 1, {0, 1} => 1}
+      result = Day03.process_move("^", context.state)
+      dbg(result)
+      assert result.position == {0, 1}
+      assert result.houses == mapset([{0, 0}, {0, 1}])
     end
 
     test "a down move", context do
       assert Day03.process_move("v", context.state).position == {0, -1}
-      assert Day03.process_move("v", context.state).houses == %{{0, 0} => 1, {0, -1} => 1}
+      assert Day03.process_move("v", context.state).houses == mapset([{0, 0}, {0, -1}])
     end
 
     test "a right move", context do
       assert Day03.process_move(">", context.state).position == {1, 0}
-      assert Day03.process_move(">", context.state).houses == %{{0, 0} => 1, {1, 0} => 1}
+      assert Day03.process_move(">", context.state).houses == mapset([{0, 0}, {1, 0}])
     end
 
     test "a left move", context do
       assert Day03.process_move("<", context.state).position == {-1, 0}
-      assert Day03.process_move("<", context.state).houses == %{{0, 0} => 1, {-1, 0} => 1}
+      assert Day03.process_move("<", context.state).houses == mapset([{0, 0}, {-1, 0}])
     end
   end
 
   defp setup_state(_context) do
     %{
       state: %{
-        houses: %{
-          {0, 0} => 1
-        },
+        houses: mapset([{0, 0}]),
         position: {0, 0}
       }
     }
+  end
+
+  defp mapset(list) do
+    MapSet.new(list)
   end
 end
